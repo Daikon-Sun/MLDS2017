ae3cfb88a1882a72801ccac3f86fed6260759f5f
eddy1021 <c.c.hsu01@gmail.com>
2017-03-09 15:00:14 +0800

    parse to raw sentences

--- hw1/README.md
+++ hw1/README.md
@@ -7,10 +7,10 @@
 ## Parse
 ### Followings should be installed and versions is suggested:
 - python 3.6.0
-- spacy 1.6.0
-- nltk 3.2.2
+- spacy 1.6.0 (if dependency tree needed)
+- nltk 3.2.2 (if dependency tree needed)
 - `python -m spacy.en.download all` should be run first to download data for
-training grammar model.(Probably forbidden for this homework)
+training grammar model.(if dependency tree needed)
 
 ### Usage
 Running following command under `/hw1` directory.
@@ -18,7 +18,16 @@ Running following command under `/hw1` directory.
 $ ./parse.py
 ```
 Make sure that training data is under `/hw1/Holmes_Training_Data/`.
-Then, parsed dependency tree will be in `/hw1/Training_Data/`.
+Then, parsed sentences will be in `/hw1/Training_Data/`.
 
-Memory usage is about `2GB`.
-Time consumed is about `40 mins`.
+If dependency tree is needed, run following command under `/hw1`.
+```
+$ ./parse_to_dt.py
+```
+
+* Normal sentences
+- Time consumed is about `2 mins`.
+
+* Dependency tree
+- Memory usage is about `2 GB`.
+- Time consumed is about `40 mins`.
--- hw1/parse.py
+++ hw1/parse.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-import os
 import re
-import spacy
 import string
 import sys
-from nltk import Tree
-
-# English language parser
-en_nlp = spacy.load('en')
-
-# Generating dependency tree
-def to_nltk_tree(node):
-  if node.n_lefts + node.n_rights > 0:
-    return Tree(node.orth_, [to_nltk_tree(child) for child in node.children])
-  else:
-    return node.orth_
 
 # Check whether given sentence is a valid English sentence
 def valid(sentence):
@@ -33,21 +20,13 @@ def valid(sentence):
   return True
   
 # Parse the given content into dependency trees
-def Parse(f,of):
+def Parse(f, of):
   content = re.sub('\n|\r', ' ', ''.join( i for i in f ))
-  content = re.split('[,.!?;]+', content)
+  content = re.split('[.!?;]+', content)
   for sentence in content:
     sent = re.sub(r'[^[a-zA-Z0-9 \']]*', '', re.sub(r' +', ' ', sentence))
     if not valid(sent): continue
-    for sent in en_nlp(sent.lower()).sents:
-      tree = to_nltk_tree(sent.root)
-      if tree == None: continue
-      if type(tree) != str:
-        sys.stdout = open('tmp','w')
-        tree.pprint()
-        sys.stdout.flush()
-        tf = open('tmp','r')
-        of.write(' '.join(line.strip() for line in tf) + '\n' )
+    of.write(' '.join(sent.lower().split()) + "\n")
 
 sys.stderr.write('start parsing...\n')
 with open('training_list','r') as file_list:
@@ -57,4 +36,3 @@ with open('training_list','r') as file_list:
       with open("Training_Data/"+file_name[21:-5]+".txt",'w') as of:
         Parse(f,of)
       sys.stderr.write('finished parsing file ' + file_name[:-1] + '\n')
-os.remove('tmp')
