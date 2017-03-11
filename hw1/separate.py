296b1d40ed1faca47a7f131b21425ee333c98f3f
awinder0230 <awinder0230@gmail.com>
2017-03-11 18:16:49 +0800

    separate pretrained word vectors

--- hw1/separate.py
+++ hw1/separate.py
@@ -10,17 +10,18 @@ sys.stderr.write('*** Seperate pre-trained word vectors from GloVe ***\n')
 
 with open("glove.6B."+sys.argv[1]+"d.txt",'r') as glove:
   with open("pretrained_word_list.txt",'w') as word_list:
-  	with open("pretrained_word_vectors.txt",'w') as vector_list:
-  		words = glove.read().split()
-  		counter = 0
-  		for w in words:
-  			if counter == 0:
-  				word_list.write(w + "\n")
-  				print("processing word:" + w)
-  				counter = counter + 1
-  			elif ( counter > 0 and counter < 50 ):
-  				vector_list.write(w + " ")
-  				counter = counter + 1
-  			else:
-  				counter = 0
-  				vector_list.write(w + "\n")
+    with open("pretrained_word_vectors_" + sys.argv[1] + "d.txt",'w') as vector_list:
+      words = glove.read().split()
+      dimension = int(sys.argv[1])
+      counter = 0
+      for w in words:
+        if counter == 0:
+          word_list.write(w + "\n")
+          print("processing word:" + w)
+          counter = counter + 1
+        elif ( counter > 0 and counter < dimension ):
+          vector_list.write(w + " ")
+          counter = counter + 1
+        else:
+          counter = 0
+          vector_list.write(w + "\n")
