c242c27200d72d2211d82a5d4ff7e5d5fcb56fa1
awinder0230 <awinder0230@gmail.com>
2017-03-13 19:56:31 +0800

    separate.py numpy version

--- hw1/separate.py
+++ hw1/separate.py
@@ -1,5 +1,6 @@
 import string
 import sys
+import numpy as np
 
 # GloVe pretrained word vectors should be put under the same directory
 
@@ -14,14 +15,21 @@ with open("glove." + sys.argv[1] + "B." + sys.argv[2] + "d.txt",'r') as glove:
       words = glove.read().split()
       dimension = int(sys.argv[2])
       counter = 0
+      wordnum = 0
+      arr = []
       for w in words:
         if counter == 0:
           word_list.write(w + "\n")
           print("processing word:" + w)
+          wordnum = wordnum + 1
           counter = counter + 1
+          arr.append([])
         elif ( counter > 0 and counter < dimension ):
-          vector_list.write(w + " ")
+          # vector_list.write(w + " ")
           counter = counter + 1
+          arr[wordnum - 1].append(float(w))
         else:
           counter = 0
-          vector_list.write(w + "\n")
+          arr[wordnum - 1].append(float(w))
+          # vector_list.write(w + "\n")
+      np.save("pretrained_word_vectors_" + sys.argv[1] + "d",np.array(arr))
