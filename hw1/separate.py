5dfb54d3df27ff092815db7b0dc4969c21f6d080
awinder0230 <awinder0230@gmail.com>
2017-03-13 20:00:47 +0800

    separate.py numpy version

--- hw1/separate.py
+++ hw1/separate.py
@@ -11,25 +11,22 @@ sys.stderr.write('*** Seperate pre-trained word vectors from GloVe ***\n')
 
 with open("glove." + sys.argv[1] + "B." + sys.argv[2] + "d.txt",'r') as glove:
   with open("pretrained_word_list.txt",'w') as word_list:
-    with open("pretrained_word_vectors_" + sys.argv[1] + "d.txt",'w') as vector_list:
-      words = glove.read().split()
-      dimension = int(sys.argv[2])
-      counter = 0
-      wordnum = 0
-      arr = []
-      for w in words:
-        if counter == 0:
-          word_list.write(w + "\n")
-          print("processing word:" + w)
-          wordnum = wordnum + 1
-          counter = counter + 1
-          arr.append([])
-        elif ( counter > 0 and counter < dimension ):
-          # vector_list.write(w + " ")
-          counter = counter + 1
-          arr[wordnum - 1].append(float(w))
-        else:
-          counter = 0
-          arr[wordnum - 1].append(float(w))
-          # vector_list.write(w + "\n")
-      np.save("pretrained_word_vectors_" + sys.argv[1] + "d",np.array(arr))
+    words = glove.read().split()
+    dimension = int(sys.argv[2])
+    counter = 0
+    wordnum = 0
+    arr = []
+    for w in words:
+      if counter == 0:
+        word_list.write(w + "\n")
+        print("processing word:" + w)
+        wordnum = wordnum + 1
+        counter = counter + 1
+        arr.append([])
+      elif ( counter > 0 and counter < dimension ):
+        counter = counter + 1
+        arr[wordnum - 1].append(float(w))
+      else:
+        counter = 0
+        arr[wordnum - 1].append(float(w))
+    np.save("pretrained_word_vectors_" + sys.argv[1] + "d",np.array(arr))
