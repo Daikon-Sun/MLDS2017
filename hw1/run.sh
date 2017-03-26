f1a57b6bb193443621f83ac4bc6e25643d261eef
b03mlds <b03mlds@localhost.localdomain>
2017-03-26 15:34:41 +0800

    change run.sh

--- hw1/run.sh
+++ hw1/run.sh
@@ -1,5 +1,8 @@
 git clone git@gitlab.com:Daikon-Sun/MLDS_models.git
 mv MLDS_models/data .
 mv MLDS_models/logs .
+mv MLDS_models/Training_Data .
 rm -rf MLDS_models
+python3 parse.py -sk -g data/glove.6B.300d.txt -t $1
 python3 deprnn.py -me 0
+mv submission.csv $2
