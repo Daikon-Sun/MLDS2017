44dbd69e9ce910b0f15f1f5279c303cd71fa868c
awinder0230 <awinder0230@gmail.com>
2017-04-16 16:14:36 +0800

    bonus.py

--- hw2/bonus.py
+++ hw2/bonus.py
@@ -8,295 +8,177 @@ import tensorflow.contrib.seq2seq as seq2seq
 from tensorflow.contrib.layers import safe_embedding_lookup_sparse as embedding_lookup_unique
 from tensorflow.contrib.rnn import LSTMCell, LSTMStateTuple, GRUCell
 
-class Seq2SeqModel():
+default_rnn_cell_type         = 1    # 0: BsicRNN, 1: BasicLSTM, 2: FullLSTM, 3: GRU
+default_video_dimension       = 4096 # dimension of each frame
+default_video_frame_num       = 80   # each video has fixed 80 frames          
+default_vocab_size            = 6089
+default_max_caption_length    = 20
+default_embedding_dimension   = 500  # embedding dimension for video and vocab
+default_hidden_units          = 1000 # according to paper
+default_batch_size            = 50
+default_layer_number          = 1
+default_max_gradient_norm     = 10
+default_dropout_keep_prob     = 0.5  # for dropout layer
+default_learning_rate         = 0.0001
+default_learning_rate_decay_factor = 1
 
-    def __init__(self, encoder_cell, decoder_cell, batch_size, video_size, vocab_size, video_embedding_size, vocab_embedding_size,
-                 bidirectional=False,
-                 attention=False):
+# default value for special vocabs
+PAD = 0
+BOS = 1
+EOS = 2
+UNK = 3
 
-        self.bidirectional = bidirectional
-        self.attention = attention
-
-        self.video_size = video_size
-        self.vocab_size = vocab_size
-        self.embedding_size = embedding_size
-
-        self.encoder_cell = encoder_cell
-        self.decoder_cell = decoder_cell
-
-        self._make_graph()
-
-    def _make_graph(self):
-        
-        self._init_placeholders()
-        self._init_decoder_train_connectors()
-        self._init_embeddings()
-        self._init_encoder()
-        self._init_decoder()
-        self._init_optimizer()
-
-    def _init_placeholders(self):
-        self.encoder_inputs = tf.placeholder(
-            tf.float32,
-            shape=(1,self.batch_size, self.video_size),
-            name='encoder_inputs',
-        )
-        self.encoder_inputs_length = tf.placeholder(
-            tf.int32,
-            shape=None,
-            name='encoder_inputs_length',
-        )
-
-        # required for training, not required for testing
-        self.decoder_targets = tf.placeholder(
-            tf.float32
-            shape=(1,self.batch_size, None),
-            name='decoder_targets'
-        )
-        self.decoder_targets_length = tf.placeholder(
-            tf.int32,
-            shape=None,
-            name='decoder_targets_length',
-        )
-
-    def _init_decoder_train_connectors(self):
-        """
-        During training, `decoder_targets`
-        and decoder logits. This means that their shapes should be compatible.
-        Here we do a bit of plumbing to set this up.
-        """
-        with tf.name_scope('DecoderTrainFeeds'):
-            _, sequence_size = tf.unstack(tf.shape(self.decoder_targets))
-
-            EOS_SLICE = tf.ones([1, batch_size], dtype=tf.float32)
-            PAD_SLICE = tf.zeros([1, batch_size], dtype=tf.float32)
-
-            self.decoder_train_inputs = tf.concat([EOS_SLICE, self.decoder_targets], axis=0)
-            self.decoder_train_length = self.decoder_targets_length + 1
-
-            decoder_train_targets = tf.concat([self.decoder_targets, PAD_SLICE], axis=0)
-            decoder_train_targets_seq_len, _ = tf.unstack(tf.shape(decoder_train_targets))
-            decoder_train_targets_eos_mask = tf.one_hot(self.decoder_train_length - 1,
-                                                        decoder_train_targets_seq_len,
-                                                        on_value=1, off_value=0,
-                                                        dtype=tf.int32)
-            decoder_train_targets_eos_mask = tf.transpose(decoder_train_targets_eos_mask, [1, 0])
-
-            # hacky way using one_hot to put EOS symbol at the end of target sequence
-            decoder_train_targets = tf.add(decoder_train_targets,
-                                           decoder_train_targets_eos_mask)
-
-            self.decoder_train_targets = decoder_train_targets
-
-            self.loss_weights = tf.ones([
-                self.batch_size,
-                tf.reduce_max(self.decoder_train_length)
-            ], dtype=tf.float32, name="loss_weights")
-
-    def _init_embeddings(self):
-        with tf.variable_scope("embedding") as scope:
-
-            initializer = tf.random_uniform_initializer(-1, 1)
-
-            self.video_embedding_matrix = tf.get_variable(
-                name="video_embedding_matrix",
-                shape=[self.video_size, self.video_embedding_size],
-                initializer=initializer,
-                dtype=tf.float32)
-            
-            self.vocab_embedding_matrix = tf.get_variable(
-                name="vocab_embedding_matrix",
-                shape=[self.vocab_size, self.vocab_embedding_size],
-                initializer=initializer,
-                dtype=tf.float32)
-
-            self.encoder_inputs_embedded = tf.nn.embedding_lookup(
-                self.video_embedding_matrix, self.encoder_inputs)
-
-            self.decoder_train_inputs_embedded = tf.nn.embedding_lookup(
-                self.vocab_embedding_matrix, self.decoder_train_inputs)
-
-    def _init_encoder(self):
-        with tf.variable_scope("Encoder") as scope:
-            (self.encoder_outputs, self.encoder_state) = (
-                tf.nn.dynamic_rnn(cell=self.encoder_cell,
-                                  inputs=self.encoder_inputs_embedded,
-                                  sequence_length=self.encoder_inputs_length,
-                                  time_major=True,
-                                  dtype=tf.float32)
-                )
-
-    def _init_decoder(self):
-        with tf.variable_scope("Decoder") as scope:
-            def output_fn(outputs):
-                return tf.contrib.layers.linear(outputs, self.vocab_size, scope=scope)
-
-            decoder_fn_train = seq2seq.simple_decoder_fn_train(encoder_state=self.encoder_state)
-            decoder_fn_inference = seq2seq.simple_decoder_fn_inference(
-                output_fn=output_fn,
-                encoder_state=self.encoder_state,
-                embeddings=self.vocab_embedding_matrix,
-                start_of_sequence_id=1,
-                end_of_sequence_id=0,
-                maximum_length=tf.reduce_max(self.encoder_inputs_length) + 3,
-                num_decoder_symbols=self.vocab_size,
-            )
-
-            (self.decoder_outputs_train,
-             self.decoder_state_train,
-             self.decoder_context_state_train) = (
-                seq2seq.dynamic_rnn_decoder(
-                    cell=self.decoder_cell,
-                    decoder_fn=decoder_fn_train,
-                    inputs=self.decoder_train_inputs_embedded,
-                    sequence_length=self.decoder_train_length,
-                    time_major=True,
-                    scope=scope,
-                )
-            )
-
-            self.decoder_logits_train = output_fn(self.decoder_outputs_train)
-            self.decoder_prediction_train = tf.argmax(self.decoder_logits_train, axis=-1, name='decoder_prediction_train')
-
-            scope.reuse_variables()
-
-            (self.decoder_logits_inference,
-             self.decoder_state_inference,
-             self.decoder_context_state_inference) = (
-                seq2seq.dynamic_rnn_decoder(
-                    cell=self.decoder_cell,
-                    decoder_fn=decoder_fn_inference,
-                    time_major=True,
-                    scope=scope,
-                )
-            )
-            self.decoder_prediction_inference = tf.argmax(self.decoder_logits_inference, axis=-1, name='decoder_prediction_inference')
-
-    def _init_optimizer(self):
-        logits = tf.transpose(self.decoder_logits_train, [1, 0, 2])
-        targets = tf.transpose(self.decoder_train_targets, [1, 0])
-        self.loss = seq2seq.sequence_loss(logits=logits, targets=targets,
-                                          weights=self.loss_weights)
-        self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
-
-    def make_train_inputs(self, input_seq, target_seq):
-        inputs_, inputs_length_ = helpers.batch(input_seq)
-        targets_, targets_length_ = helpers.batch(target_seq)
-        return {
-            self.encoder_inputs: inputs_,
-            self.encoder_inputs_length: inputs_length_,
-            self.decoder_targets: targets_,
-            self.decoder_targets_length: targets_length_,
-        }
-
-    def make_inference_inputs(self, input_seq):
-        inputs_, inputs_length_ = helpers.batch(input_seq)
-        return {
-            self.encoder_inputs: inputs_,
-            self.encoder_inputs_length: inputs_length_,
-        }
-self, encoder_cell, decoder_cell, batch_size, video_size, vocab_size, video_embedding_size, vocab_embedding_size
-
-def make_seq2seq_model(**kwargs):
-    args = dict(encoder_cell=LSTMCell(10),
-                decoder_cell=LSTMCell(20),
-                batch_size=10,
-                video_size=4096,
-                vocab_size=xxxxxxxxxxxxxxxx,
-                video_embedding_size=500,
-                vocab_embedding_size=500,
-                attention=False,
-                bidirectional=False)
-    args.update(kwargs)
-    return Seq2SeqModel(**args)
+# define mode parameters
+default_training_mode   = 0
+default_validating_mode = 1
+default_testing_mode    = 2
 
 
-def train_on_copy_task(session, model,
-                       length_from=3, length_to=8,
-                       vocab_lower=2, vocab_upper=10,
-                       batch_size=100,
-                       max_batches=5000,
-                       batches_in_epoch=1000,
-                       verbose=True):
+class S2VT(object):
 
-    batches = helpers.random_sequences(length_from=length_from, length_to=length_to,
-                                       vocab_lower=vocab_lower, vocab_upper=vocab_upper,
-                                       batch_size=batch_size)
-    loss_track = []
-    try:
-        for batch in range(max_batches+1):
-            batch_data = next(batches)
-            fd = model.make_train_inputs(batch_data, batch_data)
-            _, l = session.run([model.train_op, model.loss], fd)
-            loss_track.append(l)
+  def __init__(self, para):
 
-            if verbose:
-                if batch == 0 or batch % batches_in_epoch == 0:
-                    print('batch {}'.format(batch))
-                    print('  minibatch loss: {}'.format(session.run(model.loss, fd)))
-                    for i, (e_in, dt_pred) in enumerate(zip(
-                            fd[model.encoder_inputs].T,
-                            session.run(model.decoder_prediction_train, fd).T
-                        )):
-                        print('  sample {}:'.format(i + 1))
-                        print('    enc input           > {}'.format(e_in))
-                        print('    dec train predicted > {}'.format(dt_pred))
-                        if i >= 2:
-                            break
-                    print()
-    except KeyboardInterrupt:
-        print('training interrupted')
+    self._para = para
 
-    return loss_track
+    def single_cell():
+      if para.rnn_cell_type == 0:
+        return tf.contrib.rnn.BasicRNNCell(para.hidden_units, activation=tf.tanh)
+      elif para.rnn_cell_type == 1:
+        return tf.contrib.rnn.BasicLSTMCell(para.hidden_units, state_is_tuple=False)
+      elif para.rnn_cell_type == 2:
+        return tf.contrib.rnn.LSTMCell(para.hidden_units, use_peepholes=True, state_is_tuple=False)
+      elif para.rnn_cell_type == 3:
+        return tf.contrib.rnn.GRUCell(para.hidden_units)
 
+    # dropout layer
+    if self.is_train() and para.dropout_keep_prob < 1:
+      def rnn_cell():
+        return tf.contrib.rnn.DropoutWrapper(
+          single_cell(), output_keep_prob=para.dropout_keep_prob)
+    else:
+      def rnn_cell():
+        return single_cell()
+
+    # multi-layer within a layer
+    if para.layer_number > 1:
+      layer_1_cell = tf.contrib.rnn.MultiRNNCell(
+        [rnn_cell() for _ in range(para.layer_number)], state_is_tuple=True)
+      layer_2_cell = tf.contrib.rnn.MultiRNNCell(
+        [rnn_cell() for _ in range(para.layer_number)], state_is_tuple=True)
+    else:
+      layer_1_cell = rnn_cell()
+      layer_2_cell = rnn_cell()
+
+    # get data in batches
+    video, caption, video_len, caption_len = self.get_single_example(para)
+    videos, captions, video_lens, caption_lens = tf.train.batch([video, caption, video_len, caption_len],
+      batch_size=para.batch_size, dynamic_pad=True)
+
+    # sparse tensor cannot be sliced
+    target_captions = tf.sparse_tensor_to_dense(captions)
+
+    # video and word embeddings
+    with tf.variable_scope('word_embedding'):
+      word_embedding_w = tf.get_variable('word_embed',
+        [para.vocab_size, para.embedding_dimension])
+
+    with tf.variable_scope('video_embedding'):
+      video_embedding_W = tf.get_variable('video_embed',
+        [para.video_dimension, para.embedding_dimension])
+
+    # embed videos and captions
+    embed_video_inputs = tf.matmul(videos, video_embedding_W)
+    embed_targets      = tf.nn.embedding_lookup(word_embedding_w, target_captions)
+
+    # apply dropout to inputs
+    if self.is_train() and para.dropout_keep_prob < 1:
+      embed_video_inputs = tf.nn.dropout(embed_video_inputs, para.dropout_keep_prob)
+
+    # Initial state of the LSTM memory.
+    state_1 = tf.zeros([para.batch_size, layer_1_cell.state_size])
+    state_2 = tf.zeros([para.batch_size, layer_2_cell.state_size])
+    probabilities = []
+    loss = 0.0
+
+    # paddings for 1st and 2nd layers
+    layer_1_padding = tf.zeros([para.batch_size, para.embedding_dimension])
+    layer_2_padding = tf.zeros([para.batch_size, para.embedding_dimension])
+
+
+    # ====================== ENCODING STAGE ======================
+    for i in range(0, para.video_frame_num):
+      if i > 0:
+        tf.get_variable_scope().reuse_variables()
+      with tf.variable_scope("layer_1"):
+        layer_1_output, state_1 = layer_1_cell(embed_video_inputs[:,i,:], state_1) # batch_size x frame_num x embed_dim
+      with tf.variable_scope("layer_2"):
+        layer_2_output, state_2 = layer_2_cell(tf.concat([layer_2_padding, layer_1_output], 1))
+
+    # ====================== ENCODING STAGE ======================
+    for i in range(0, para.max_caption_length):
+
+
+
+  # ======================== end of __init__ ======================== #
+
+  def is_train(self): return self._para.mode == 0
+  def is_valid(self): return self._para.mode == 1
+  def  is_test(self): return self._para.mode == 2
+
+  def get_single_example(self, para):
+    # first construct a queue containing a list of filenames.
+    filename_queue = tf.train.string_input_producer([para.filename], num_epochs=None)
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(
+      serialized_example,
+      features={
+        'video': tf.FixedLenFeature([para.video_frame_num*para.video_dimension], tf.float32),
+        'caption': tf.VarLenFeature(tf.int64)
+      })
+    video = tf.reshape(feature['video'], [para.video_frame_num, para.video_dimension])
+    caption = features['caption']
+    return video, caption, tf.shape(video)[0], tf.shape(caption)[0]
+
+default_rnn_cell_type         = 1    # 0: BsicRNN, 1: BasicLSTM, 2: FullLSTM, 3: GRU
+default_video_dimension       = 4096 # dimension of each frame
+default_video_frame_num       = 80   # each video has fixed 80 frames          
+default_vocab_size            = 6089
+default_max_caption_length    = 20
+default_embedding_dimension   = 500  # embedding dimension for video and vocab
+default_hidden_units          = 1000 # according to paper
+default_batch_size            = 50
+default_layer_number          = 1
+default_max_gradient_norm     = 10
+default_dropout_keep_prob     = 0.5  # for dropout layer
+default_learning_rate         = 0.0001
+default_learning_rate_decay_factor = 1
 
 if __name__ == '__main__':
-    import sys
+  argparser = argparse.ArgumentParser(description='S2VT encoder and decoder')
+  argparser.add_argument('-type', '--rnn_cell_type',
+    type=int, default=default_rnn_cell_type,
+    help='rnn cell type: 0->BasicRNN, 1->BasicLSTM, 2->FullLSTM, 3->GRU')
+  argparser.add_argument('-vd', '--video_dimension',
+    type=int, default=default_video_dimension,
+    help='video dimension (default:%d)' %default_video_dimension)
+  argparser.add_argument('-vfn', '--video_frame_num',
+    type=int, default=default_video_frame_num,
+    help='video frame numbers (default:%d)' %default_video_frame_num)
+  argparser.add_argument('-vocab_size', '--vocab_size',
+    type=int, default=default_vocab_size,
+    help='vocab size (default:%d)' %default_vocab_size)
+  argparser.add_argument('-mcl', '--max_caption_length',
+    type=int, default=default_max_caption_length,
+    help='maximum output caption length (default:%d)' %default_max_caption_length)
+  argparser.add_argument('-ed', '--embedding_dimension',
+    type=int, default=default_embedding_dimension,
+    help='embedding dimension of video and caption (default:%d)' %default_embedding_dimension)
+  argparser.add_argument('-hu', '--hidden_units',
+    type=int, default=default_hidden_units,
+    help='hidden units of rnn cell (default:%d)' %default_hidden_units)
+  argparser.add_argument('-bs', '--batch_size',
+    type=int, default=default_batch_size,
+    help='batch size (default:%d)' %default_batch_size)
+  args = argparser.parse_args()
 
-    if 'fw-debug' in sys.argv:
-        tf.reset_default_graph()
-        with tf.Session() as session:
-            model = make_seq2seq_model(debug=True)
-            session.run(tf.global_variables_initializer())
-            session.run(model.decoder_prediction_train)
-            session.run(model.decoder_prediction_train)
-
-    elif 'fw-inf' in sys.argv:
-        tf.reset_default_graph()
-        with tf.Session() as session:
-            model = make_seq2seq_model()
-            session.run(tf.global_variables_initializer())
-            fd = model.make_inference_inputs([[5, 4, 6, 7], [6, 6]])
-            inf_out = session.run(model.decoder_prediction_inference, fd)
-            print(inf_out)
-
-    elif 'train' in sys.argv:
-        tracks = {}
-
-        tf.reset_default_graph()
-
-        with tf.Session() as session:
-            model = make_seq2seq_model(attention=True)
-            session.run(tf.global_variables_initializer())
-            loss_track_attention = train_on_copy_task(session, model)
-
-        tf.reset_default_graph()
-
-        with tf.Session() as session:
-            model = make_seq2seq_model(attention=False)
-            session.run(tf.global_variables_initializer())
-            loss_track_no_attention = train_on_copy_task(session, model)
-
-        import matplotlib.pyplot as plt
-        plt.plot(loss_track)
-        print('loss {:.4f} after {} examples (batch_size={})'.format(loss_track[-1], len(loss_track)*batch_size, batch_size))
-
-    else:
-        tf.reset_default_graph()
-        session = tf.InteractiveSession()
-        model = make_seq2seq_model(debug=False)
-        session.run(tf.global_variables_initializer())
-
-        fd = model.make_inference_inputs([[5, 4, 6, 7], [6, 6]])
-
-        inf_out = session.run(model.decoder_prediction_inference, fd)
