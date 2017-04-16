bb80bc8125d937a812b41bfbedc58daf3a6e5cda
daikon <b03901056@ntu.edu.tw>
2017-04-16 16:15:44 +0800

    testing

--- hw2/seq_to_seq.py
+++ hw2/seq_to_seq.py
@@ -10,134 +10,6 @@ from tensorflow.contrib.seq2seq import sequence_loss as sequence_loss
 from tensorflow.contrib.layers import legacy_fully_connected as fully_connected
 from nltk.tokenize import word_tokenize
 
-#default values (in alphabetic order)
-default_batch_size = 1024
-default_data_dir = './parsed_data/'
-default_embed_dim = 500
-default_hidden_size = 300
-default_info_epoch = 1
-default_init_scale = 0.01
-default_keep_prob = 0.8
-default_layer_num = 2
-default_learning_rate = 0.0005
-default_rnn_type = 2
-default_max_grad_norm = 1
-default_max_epoch = 5000
-default_num_sampled = 2000
-default_optimizer = 4
-default_output_filename = './submission.csv'
-#default_softmax_loss = 0
-default_video_dim = 4096
-default_video_len = 80
-#default_train_num = 522
-#default_wordvec_src = 3
-optimizers = [tf.train.GradientDescentOptimizer, tf.train.AdadeltaOptimizer,
-              tf.train.AdagradOptimizer, tf.train.MomentumOptimizer,
-              tf.train.AdamOptimizer, tf.train.RMSPropOptimizer]
-
-#functions for arguments of unsupported types
-def restricted_float(x):
-    x = float(x)
-    if x < 0.0 or x > 1.0:
-        raise argparse.ArgumentTypeError('%r not in range [0.0, 1.0]'%x)
-    return x
-
-##argument parser
-parser = argparse.ArgumentParser(description=
-                                 'seq2seq for video caption generation')
-parser.add_argument('-ed', '--embed_dim',
-                    type=int, default=default_embed_dim,
-                    nargs='?', help='Embedding dimension of vocabularies. '
-                    '(default:%d)'%default_embed_dim)
-parser.add_argument('-vl', '--video_len',
-                    type=int, default=default_video_len,
-                    nargs='?', help='Number of a frame in a video. '
-                    '(default:%d)'%default_video_len)
-parser.add_argument('-vd', '--video_dim',
-                    type=int, default=default_video_dim,
-                    nargs='?', help='Dimension of a frame from a video. '
-                    '(default:%d)'%default_video_dim)
-parser.add_argument('-ln', '--layer_num',
-                    type=int, default=default_layer_num,
-                    nargs='?', help='Number of rnn layer. (default:%d)'
-                    %default_layer_num)
-parser.add_argument('--info_epoch',
-                    type=int, default=default_info_epoch,
-                    nargs='?', help='Print information every info_epoch.'
-                    '(default:%d)'%default_info_epoch)
-parser.add_argument('-ns', '--num_sampled',
-                    type=int, default=default_num_sampled,
-                    nargs='?', help='Number of classes to be sampled while'
-                    'calculating loss (not with full softmax).'
-                    '(default:%d)'%default_num_sampled)
-parser.add_argument('-opt', '--optimizer',
-                    type=int, default=default_optimizer,
-                    nargs='?', choices=range(0, 6), help='Optimizers -->'
-                    '[0: GradientDescent], [1:Adadelta], [2:Adagrad],'
-                    '[3:Momentum], [4:Adam], [5:RMSProp]. (default:%d)'
-                    %default_optimizer)
-#parser.add_argument('-sl', '--softmax_loss',
-#                    type=int, default=default_softmax_loss,
-#                    nargs='?', choices=range(0, 3), help='Type of softmax'
-#                    'function --> [0:full softmax], [1:sampled softmax],'
-#                    '[nce loss]. (default:%d)'%default_softmax_loss)
-parser.add_argument('-rt', '--rnn_type',
-                    type=int, default=default_rnn_type,
-                    nargs='?', choices=range(0, 4), help='Type of rnn cell -->'
-                    '[0:Basic], [1:basic LSTM], [2:full LSTM], [3:GRU].'
-                    '(default:%d)'%default_rnn_type)
-parser.add_argument('-lr', '--learning_rate',
-                    type=float, default=default_learning_rate,
-                    nargs='?', help='Value of initial learning rate.'
-                    '(default:%r)'%default_learning_rate)
-parser.add_argument('-mgn', '--max_grad_norm',
-                    type=float, default=default_max_grad_norm,
-                    nargs='?', help='Maximum gradient norm allowed. (default:%r)'
-                    %default_max_grad_norm)
-parser.add_argument('-is', '--init_scale',
-                    type=float, default=default_init_scale,
-                    nargs='?', help='initialize scale. (default:%r)'
-                    %default_init_scale)
-parser.add_argument('-me', '--max_epoch',
-                    type=int, default=default_max_epoch,
-                    nargs='?', help='Maximum epoch to be trained.'
-                    '(default:%d)'%default_max_epoch)
-#parser.add_argument('-tn', '--train_num',
-#                    type=int, default=default_train_num,
-#                    nargs='?', help='Number of files out of the total 522'
-#                    'files to be trained. (default:%d)' %default_train_num)
-parser.add_argument('-kp', '--keep_prob',
-                    type=restricted_float,
-                    default=default_keep_prob, nargs='?', help=
-                    'Keeping-Probability for dropout layer.'
-                    '(default:%r)'%default_keep_prob)
-parser.add_argument('-bs', '--batch_size',
-                    type=int, default=default_batch_size,
-                    nargs='?', help='Mini-batch size while training.'
-                    '(default:%d)'%default_batch_size)
-parser.add_argument('-hs', '--hidden_size',
-                    type=int, default=default_hidden_size,
-                    nargs='?', help='Dimension of hidden layer.'
-                    '(default:%d)'%default_hidden_size)
-#parser.add_argument('-dd', '--data_dir',
-#                    type=str, default=default_data_dir, nargs='?',
-#                    help='Directory where the data are placed.'
-#                    '(default:%s)'%default_data_dir)
-#parser.add_argument('-of', '--output_filename',
-#                    type=str, default=default_output_filename, nargs='?',
-#                    help='Filename of the final prediction.'
-#                    '(default:%s)'%default_output_filename)
-args = parser.parse_args()
-vocab = open('train_tfrdata/vocab.txt', 'r').read().splitlines()
-vocab = dict([[i, word] for i, word in enumerate(vocab)])
-args.vocab_size = len(vocab)
-print('vocab_size = %d'%args.vocab_size)
-
-#calculate real epochs
-print('training with about %.3f epochs!'
-      %((args.batch_size*args.max_epoch)/24232))
-
-
 class S2S(object):
 
   def __init__(self, para):
@@ -184,7 +56,6 @@ class S2S(object):
       videos, v_lens =\
           tf.train.batch([video, v_len],
                          batch_size=para.batch_size, dynamic_pad=True)
-    self._val = videos
     v_lens = tf.to_int32(v_lens)
 
     #word_id to vector
@@ -200,10 +71,9 @@ class S2S(object):
       inputs = tf.nn.dropout(inputs, para.keep_prob)
 
     #use dynamic_rnn to build dynamic-time-step rnn
-    encoder_outputs, encoder_states =\
+    _, encoder_states =\
       tf.nn.dynamic_rnn(encoder_cell, inputs,
                         sequence_length=v_lens, dtype=tf.float32)
-    encoder_output = tf.reshape(encoder_outputs, [-1, para.hidden_size])
 
     #pass_states =\
     #  tuple([LSTMStateTuple(c=tf.zeros(tf.shape(encoder_states[i].c)),
@@ -229,7 +99,7 @@ class S2S(object):
           maximum_length=30,
           num_decoder_symbols=para.vocab_size)
       with tf.variable_scope('decode', reuse=True):
-        decoder_logits, tmp_states, _ =\
+        decoder_logits, _, _ =\
           seq2seq.dynamic_rnn_decoder(cell=decoder_cell,
                                     decoder_fn=decoder_fn_inference)
       self._prob = tf.nn.softmax(decoder_logits)
@@ -237,7 +107,7 @@ class S2S(object):
 
     decoder_fn_train = seq2seq.simple_decoder_fn_train(pass_states)
     with tf.variable_scope('decode', reuse=None):
-      decoder_outputs, tmp_states, _ =\
+      decoder_outputs, _, _ =\
         seq2seq.dynamic_rnn_decoder(cell=decoder_cell,
                                     decoder_fn=decoder_fn_train,
                                     inputs=decoder_in_embed,
@@ -278,7 +148,7 @@ class S2S(object):
       filelist = open('training_list.txt', 'r').read().splitlines()
       filenames = [ 'train_tfrdata/'+fl+'.tfr' for fl in filelist ]
 
-    f_queue = tf.train.string_input_producer(filenames, shuffle=False)
+    f_queue = tf.train.string_input_producer(filenames, shuffle=True)
     reader = tf.TFRecordReader()
 
     _, serialized_example = reader.read(f_queue)
@@ -315,7 +185,7 @@ def run_epoch(sess, model, args):
   if not model.is_test():
     fetches['cost'] = model.cost
     #fetches['prob'] = model.prob
-    fetches['val'] = model.val
+    #fetches['val'] = model.val
     if model.is_train():
       fetches['eval'] = model.eval
     #fetches['inputs'] = model.inputs
@@ -329,15 +199,16 @@ def run_epoch(sess, model, args):
 
   else:
     fetches['prob'] = model.prob
-    fetches['val'] = model.val
+    #fetches['val'] = model.val
 
     vals = sess.run(fetches)
     prob = vals['prob']
+    #np.save('tmp_1', prob)
     #for var, val in zip(fetches['val'], vals['val']):
     #  print(var.name)
-    print(vals['val'])
-    print(vals['val'].shape)
-    #print(prob)
+    #print(vals['val'])
+    #print(vals['val'].shape)
+    print(prob.shape)
 
     bests = []
     for i in range(prob.shape[0]):
@@ -345,27 +216,159 @@ def run_epoch(sess, model, args):
       for j in range(prob.shape[1]):
         mx_i = np.argmax(prob[i, j, :])
         mx_v = prob[i, j, mx_i]
-        if mx_v < 1e-9:
+        print(mx_v, mx_i)
+        if mx_i == 3:
           break
         ps.append(np.log(mx_v))
         ans.append(vocab[mx_i])
+      print('-'*80)
       mn = np.mean(ps)
       bests.append(ans)
     print(bests)
 
 if __name__ == '__main__':
-  with open('MLDS_hw2_data/training_label.json', 'r') as label_json:
-    labels = json.load(label_json)
-    captions = [ [ word_tokenize(sent.lower()) for sent in label['caption'] ]
-               for label in labels ]
-    sents = [ sent for caption in captions for sent in caption ]
-    vocabs = set([ word for sent in sents for word in sent ])
-    vocabs = ['<pad>', '<unk>', '<bos>', '<eos>'] + list(vocabs)
-    dct = dict([ (word, i) for i, word in enumerate(vocabs)])
 
-  with open('train_tfrdata/vocab.txt', 'w') as f:
-    for word in dct.keys():
-      f.write(word+'\n')
+  #default values (in alphabetic order)
+  default_batch_size = 1024
+  default_data_dir = './parsed_data/'
+  default_embed_dim = 500
+  default_hidden_size = 300
+  default_info_epoch = 1
+  default_init_scale = 0.01
+  default_keep_prob = 0.8
+  default_layer_num = 2
+  default_learning_rate = 0.0005
+  default_rnn_type = 2
+  default_max_grad_norm = 1
+  default_max_epoch = 5000
+  default_num_sampled = 2000
+  default_optimizer = 4
+  default_output_filename = './submission.csv'
+  #default_softmax_loss = 0
+  default_video_dim = 4096
+  default_video_len = 80
+  #default_train_num = 522
+  #default_wordvec_src = 3
+  optimizers = [tf.train.GradientDescentOptimizer, tf.train.AdadeltaOptimizer,
+                tf.train.AdagradOptimizer, tf.train.MomentumOptimizer,
+                tf.train.AdamOptimizer, tf.train.RMSPropOptimizer]
+
+  #functions for arguments of unsupported types
+  def restricted_float(x):
+      x = float(x)
+      if x < 0.0 or x > 1.0:
+          raise argparse.ArgumentTypeError('%r not in range [0.0, 1.0]'%x)
+      return x
+
+  ##argument parser
+  parser = argparse.ArgumentParser(description=
+                                   'seq2seq for video caption generation')
+  parser.add_argument('-ed', '--embed_dim',
+                      type=int, default=default_embed_dim,
+                      nargs='?', help='Embedding dimension of vocabularies. '
+                      '(default:%d)'%default_embed_dim)
+  parser.add_argument('-vl', '--video_len',
+                      type=int, default=default_video_len,
+                      nargs='?', help='Number of a frame in a video. '
+                      '(default:%d)'%default_video_len)
+  parser.add_argument('-vd', '--video_dim',
+                      type=int, default=default_video_dim,
+                      nargs='?', help='Dimension of a frame from a video. '
+                      '(default:%d)'%default_video_dim)
+  parser.add_argument('-ln', '--layer_num',
+                      type=int, default=default_layer_num,
+                      nargs='?', help='Number of rnn layer. (default:%d)'
+                      %default_layer_num)
+  parser.add_argument('--info_epoch',
+                      type=int, default=default_info_epoch,
+                      nargs='?', help='Print information every info_epoch.'
+                      '(default:%d)'%default_info_epoch)
+  parser.add_argument('-ns', '--num_sampled',
+                      type=int, default=default_num_sampled,
+                      nargs='?', help='Number of classes to be sampled while'
+                      'calculating loss (not with full softmax).'
+                      '(default:%d)'%default_num_sampled)
+  parser.add_argument('-opt', '--optimizer',
+                      type=int, default=default_optimizer,
+                      nargs='?', choices=range(0, 6), help='Optimizers -->'
+                      '[0: GradientDescent], [1:Adadelta], [2:Adagrad],'
+                      '[3:Momentum], [4:Adam], [5:RMSProp]. (default:%d)'
+                      %default_optimizer)
+  #parser.add_argument('-sl', '--softmax_loss',
+  #                    type=int, default=default_softmax_loss,
+  #                    nargs='?', choices=range(0, 3), help='Type of softmax'
+  #                    'function --> [0:full softmax], [1:sampled softmax],'
+  #                    '[nce loss]. (default:%d)'%default_softmax_loss)
+  parser.add_argument('-rt', '--rnn_type',
+                      type=int, default=default_rnn_type,
+                      nargs='?', choices=range(0, 4), help='Type of rnn cell -->'
+                      '[0:Basic], [1:basic LSTM], [2:full LSTM], [3:GRU].'
+                      '(default:%d)'%default_rnn_type)
+  parser.add_argument('-lr', '--learning_rate',
+                      type=float, default=default_learning_rate,
+                      nargs='?', help='Value of initial learning rate.'
+                      '(default:%r)'%default_learning_rate)
+  parser.add_argument('-mgn', '--max_grad_norm',
+                      type=float, default=default_max_grad_norm,
+                      nargs='?', help='Maximum gradient norm allowed. (default:%r)'
+                      %default_max_grad_norm)
+  parser.add_argument('-is', '--init_scale',
+                      type=float, default=default_init_scale,
+                      nargs='?', help='initialize scale. (default:%r)'
+                      %default_init_scale)
+  parser.add_argument('-me', '--max_epoch',
+                      type=int, default=default_max_epoch,
+                      nargs='?', help='Maximum epoch to be trained.'
+                      '(default:%d)'%default_max_epoch)
+  #parser.add_argument('-tn', '--train_num',
+  #                    type=int, default=default_train_num,
+  #                    nargs='?', help='Number of files out of the total 522'
+  #                    'files to be trained. (default:%d)' %default_train_num)
+  parser.add_argument('-kp', '--keep_prob',
+                      type=restricted_float,
+                      default=default_keep_prob, nargs='?', help=
+                      'Keeping-Probability for dropout layer.'
+                      '(default:%r)'%default_keep_prob)
+  parser.add_argument('-bs', '--batch_size',
+                      type=int, default=default_batch_size,
+                      nargs='?', help='Mini-batch size while training.'
+                      '(default:%d)'%default_batch_size)
+  parser.add_argument('-hs', '--hidden_size',
+                      type=int, default=default_hidden_size,
+                      nargs='?', help='Dimension of hidden layer.'
+                      '(default:%d)'%default_hidden_size)
+  #parser.add_argument('-dd', '--data_dir',
+  #                    type=str, default=default_data_dir, nargs='?',
+  #                    help='Directory where the data are placed.'
+  #                    '(default:%s)'%default_data_dir)
+  #parser.add_argument('-of', '--output_filename',
+  #                    type=str, default=default_output_filename, nargs='?',
+  #                    help='Filename of the final prediction.'
+  #                    '(default:%s)'%default_output_filename)
+  args = parser.parse_args()
+
+  #calculate real epochs
+  print('training with about %.3f epochs!'
+        %((args.batch_size*args.max_epoch)/25000))
+
+  dct_file = 'train_tfrdata/vocab.txt'
+  if tf.gfile.Exists(dct_file):
+    vocab = open(dct_file, 'r').read().splitlines()
+    vocab = dict([[i, word] for i, word in enumerate(vocab)])
+    args.vocab_size = len(vocab)
+    print('vocab_size = %d'%args.vocab_size)
+  else:
+    with open('MLDS_hw2_data/training_label.json', 'r') as label_json:
+      labels = json.load(label_json)
+      captions = [ [ word_tokenize(sent.lower()) for sent in label['caption'] ]
+                 for label in labels ]
+      sents = [ sent for caption in captions for sent in caption ]
+      vocabs = set([ word for sent in sents for word in sent ])
+      vocabs = ['<pad>', '<unk>', '<bos>', '<eos>'] + list(vocabs)
+      dct = dict([ (word, i) for i, word in enumerate(vocabs)])
+    with open('train_tfrdata/vocab.txt', 'w') as f:
+      for word in dct.keys():
+        f.write(word+'\n')
 
   with open('MLDS_hw2_data/training_label.json', 'r') as label_json:
     labels = json.load(label_json)
