20b8d659b35845d9ab84c285094d98596c85a751
Daikon-Sun <b03901056@ntu.edu.tw>
2017-04-27 02:35:29 +0800

    flexible run.sh

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,7 +1,13 @@
 git clone https://Daikon-Sun@gitlab.com/Daikon-Sun/MLDS_hw2_basic_model.git
 cd MLDS_hw2_basic_model
-if [[ "$1" != /* ]]; then $1 = "../"$1 fi
-if [[ "$2" != /* ]]; then $2 = "../"$2 fi
-python3 seq_to_seq.py -at -bi -ss -ed 4096 -me 0 -tid "../"$1 -fp "../"$2
+if [[ "$1" = /* ]]
+  then tid=$1 
+  else tid="../"$1
+fi
+if [[ "$2" = /* ]]
+  then feat=$2 
+  else feat="../"$2
+fi
+python3 seq_to_seq.py -at -bi -ss -ed 4096 -me 0 -tid $tid -fp $feat
 mv output.json ../
 cd ..
