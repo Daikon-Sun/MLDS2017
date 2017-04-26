d2fc7406d6547184dd7866a8aeb2d2c12377e566
Daikon <b03901056@ntu.edu.tw>
2017-04-27 02:21:25 +0800

    fix run.sh bugs

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,5 +1,5 @@
 git clone https://Daikon-Sun@gitlab.com/Daikon-Sun/MLDS_hw2_basic_model.git
 cd MLDS_hw2_basic_model
-python3 seq_to_seq.py -at -bi -ss -ed 4096 -me 0 -tid $1 -fp $2
+python3 seq_to_seq.py -at -bi -ss -ed 4096 -me 0 -tid "../"$1 -fp "../"$2
 mv output.json ../
 cd ..
