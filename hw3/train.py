8f4c1257442ebb6a6ee489501d2c58997daba1d6
Daikon <b03901056@ntu.edu.tw>
2017-05-24 14:41:03 +0800

    fix

--- hw3/train.py
+++ hw3/train.py
@@ -71,8 +71,8 @@ def main():
     g_optim = tf.train.AdamOptimizer(args.learning_rate, beta1 = args.beta1).minimize(loss['g_loss'], var_list=variables['g_vars'])
 
   config = tf.ConfigProto()
-  config.gpu_options.allow_growth = True
   config.gpu_options.per_process_gpu_memory_fraction = 0.4
+  config.gpu_options.allow_growth = True
   sess = tf.Session(config=config)
   init = tf.global_variables_initializer()
   sess.run(init)
@@ -139,8 +139,9 @@ def main():
         save_for_vis(args.data_set, args.method_dir, real_images,
                      gen, image_files)
         save_path =\
-          saver.save(sess, join(args.method_dir, 'Models', '/Models/latest_'
-                                'model_{}_temp.ckpt'.format(args.data_set)))
+          saver.save(sess, join(args.data_set, args.method_dir, 'Models',
+                                'latest_model_'
+                                '{}_temp.ckpt'.format(args.data_set)))
     if i%5 == 0:
       save_path =\
         saver.save(sess, join(args.method_dir, 'Models', 'model_after_'
@@ -177,13 +178,13 @@ def save_for_vis(data_set, method_dir, real_images,
     real_image_255 = np.zeros( (64,64,3), dtype=np.uint8)
     real_images_255 = (real_images[i,:,:,:])
     scipy.misc.imsave(\
-      join(method_dir,
-           'samples/{}_{}.jpg'.format(i, image_files[i].split('/')[-1] )),
+      join(data_set, method_dir, 'samples',
+           '{}_{}.jpg'.format(i, image_files[i].split('/')[-1] )),
       real_images_255)
     fake_image_255 = np.zeros((64,64,3), dtype=np.uint8)
     fake_images_255 = (generated_images[i,:,:,:])
-    scipy.misc.imsave(join(data_set, method_dir,
-                           'samples/fake_image_{}.jpg'.format(i)),
+    scipy.misc.imsave(join(data_set, method_dir, 'samples'
+                           'fake_image_{}.jpg'.format(i)),
                       fake_images_255)
 
 def get_training_batch(batch_no, batch_size, image_size, z_dim,
