b96ef070f5bcefa6f949498aa44e65857d3f5f4c
Daikon-Sun <b03901056@ntu.edu.tw>
2017-06-10 16:13:13 +0800

    update

--- final/GoogLeNet/test_googlenet.py
+++ final/GoogLeNet/test_googlenet.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+import os, sys, time, copy, h5py, argparse
 import numpy as np
-import tensorflow as tf
-import vgg19, os, utils, time, argparse
-from scipy.misc import imread, imresize
 from tf_cnnvis import *
+import tensorflow as tf
+from subprocess import call
+from scipy.misc import imread, imresize
 
-X = tf.placeholder(tf.float32, shape = [None, 224, 224, 3])
-vgg = vgg19.Vgg19()
-vgg.build(X)
+with tf.gfile.FastGFile('tensorflow_inception_graph.pb', 'rb') as f:
+  graph_def = tf.GraphDef()
+  graph_def.ParseFromString(f.read())
+t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+imagenet_mean = 117.0
+t_preprocessed = t_input-imagenet_mean
+tf.import_graph_def(graph_def, {'input' : t_preprocessed})
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--prefix', '-p', type=str, default='',
@@ -18,19 +23,18 @@ parser.add_argument('--method', '-m', type=int, default=0,
                     choices=(0, 1), help='methods to visualize')
 parser.add_argument('--image', '-i', type=str, default='NULL',
                     help='input image')
+
 args = parser.parse_args()
 layers = list(args.layers)
 logdir = args.prefix+'_Log'
 outdir = args.prefix+'_Output'
-im = np.expand_dims(imresize(imresize(imread(
-  os.path.join("../imgs", args.image)), (256, 256)), (224, 224)), axis = 0)
-
+im = np.expand_dims(imresize(imread(os.path.join("../imgs", args.image)), (224, 224)), axis = 0)
 visual_func = [deconv_visualization, activation_visualization]
 
 start = time.time()
 is_success =\
   visual_func[args.method](graph_or_path=tf.get_default_graph(),
-                           value_feed_dict={X : im}, layers=layers,
+                           value_feed_dict={t_input : im}, layers=layers,
                            path_logdir=logdir, path_outdir=outdir)
 start = time.time() - start
 print("Total Time = %f" % (start))
--- final/Vgg16/test_vgg16.py
+++ final/Vgg16/test_vgg16.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 import numpy as np
 import tensorflow as tf
-import vgg16, os, utils, time
+import vgg16, os, utils, time, argparse
 from scipy.misc import imread, imresize
 from tf_cnnvis import *
 
@@ -8,13 +9,28 @@ X = tf.placeholder(tf.float32, shape = [None, 224, 224, 3])
 vgg = vgg16.Vgg16()
 vgg.build(X)
 
-layers = ['r', 'p', 'c']
+parser = argparse.ArgumentParser()
+parser.add_argument('--prefix', '-p', type=str, default='',
+                    help='prefix of log and output directory')
+parser.add_argument('--layers', '-l', type=str, default='rpc',
+                    help='layers to be visualized')
+parser.add_argument('--method', '-m', type=int, default=0,
+                    choices=(0, 1), help='methods to visualize')
+parser.add_argument('--image', '-i', type=str, default='NULL',
+                    help='input image')
+args = parser.parse_args()
+layers = list(args.layers)
+logdir = args.prefix+'_Log'
+outdir = args.prefix+'_Output'
 im = np.expand_dims(imresize(imresize(imread(
-  os.path.join("../imgs", "car.jpg")), (256, 256)), (224, 224)), axis = 0)
+  os.path.join("../imgs", args.image)), (256, 256)), (224, 224)), axis = 0)
+
+visual_func = [deconv_visualization, activation_visualization]
 
 start = time.time()
-is_success = deconv_visualization(graph_or_path=tf.get_default_graph(),
-                                  value_feed_dict={X : im}, layers=layers,
-                                  path_logdir="./Log", path_outdir="./Output")
+is_success =\
+  visual_func[args.method](graph_or_path=tf.get_default_graph(),
+                           value_feed_dict={X : im}, layers=layers,
+                           path_logdir=logdir, path_outdir=outdir)
 start = time.time() - start
 print("Total Time = %f" % (start))
