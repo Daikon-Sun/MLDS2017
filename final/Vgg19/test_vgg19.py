7a85c22ab3dc4ed902be304df00b24dc0d4af2be
Daikon-Sun <b03901056@ntu.edu.tw>
2017-06-10 14:29:31 +0800

    override

--- final/Vgg19/test_vgg19.py
+++ final/Vgg19/test_vgg19.py
@@ -1,24 +1,20 @@
 import numpy as np
 import tensorflow as tf
+import vgg19, os, utils, time
+from scipy.misc import imread, imresize
+from tf_cnnvis import *
 
-import vgg19
-import utils
+X = tf.placeholder(tf.float32, shape = [None, 224, 224, 3])
+vgg = vgg19.Vgg19()
+vgg.build(X)
 
-img = utils.load_image("./test_data/tiger.jpeg")
-
-batch = img.reshape((1, 224, 224, 3))
-
-# with tf.Session(config=tf.ConfigProto(gpu_options=(tf.GPUOptions(per_process_gpu_memory_fraction=0.7)))) as sess:
-with tf.device('/cpu:0'):
-  with tf.Session() as sess:
-    images = tf.placeholder("float", batch.shape)
-    feed_dict = {images : batch}
-
-    vgg = vgg19.Vgg19()
-    with tf.name_scope("content_vgg"):
-      vgg.build(images)
-
-      prob = sess.run(vgg.prob, feed_dict=feed_dict)
-      print(prob)
-      utils.print_prob(prob[0], './synset.txt')
-      utils.print_prob(prob[1], './synset.txt')
+layers = ['r', 'p', 'c']
+im = np.expand_dims(imresize(imresize(imread(
+  os.path.join("../imgs", "keyboard.jpg")), (256, 256)), (224, 224)), axis = 0)
+start = time.time()
+is_success = deconv_visualization(graph_or_path=tf.get_default_graph(),
+                                  value_feed_dict={X : im}, layers=layers,
+                                  path_logdir="./Keyboard_Log",
+                                  path_outdir="./Keyboard_Output")
+start = time.time() - start
+print("Total Time = %f" % (start))
