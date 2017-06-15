9fea78491e90a74691d33e7d8175d3b75a0af685
Daikon-Sun <b03901056@ntu.edu.tw>
2017-06-15 19:19:26 +0800

    fix run.sh

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,11 +1,11 @@
 if [ "${1}" == "S2S" ]; then
   git clone https://gitlab.com/Daikon-Sun/MLDS_hw4_S2S_model.git
   mkdir works
-  mkdir works/data
-  mkdir works/data/train
-  mkdir works/data/test
   mkdir works/movie_subtitles
   mkdir works/movie_subtitles/nn_models
+  mkdir works/movie_subtitles/data
+  mkdir works/movie_subtitles/data/train
+  mkdir works/movie_subtitles/data/test
   cp MLDS_hw4_S2S_model/* works/movie_subtitles/nn_models
 
   mv $2 works/movie_subtitles/data/test/test_set.txt
